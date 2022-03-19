99c2ea5c58f2d64634c19d993861f6c392aa9d2d
Ngô Xuân Huy - 60135752 <itnxhuy1806@gmail.com>
2022-03-19 13:47:34 +0700

    add function using while

--- AONJ - 4 - sum of 2 element in array.js
+++ AONJ - 4 - sum of 2 element in array.js
@@ -2,13 +2,34 @@ let array = [-1, 0, 2, 3, 5, 8]
 
 let targetSum = 2
 
-function getSumof2element() {
-     let result = [];
-     let a = [...array]
-     array.forEach((e_array) => {
-          a = a.filter((e_a) => e_a != e_array)
-          a.forEach((e_a) => { if (e_a + e_array == 2) result.push([e_array, e_a]) })
+function getSumof2element(arr, target) { //Time complexity O(n^2), Space complexity O(n)
+     let result = []
+     let a = [...arr]
+     arr.forEach((e_arr) => {
+          a = a.filter((e_a) => e_a != e_arr)
+          a.forEach((e_a) => { if (e_a + e_arr == target) result.push([e_arr, e_a]) })
      })
      return result
 }
-console.log(getSumof2element());
+console.log(getSumof2element(array,targetSum));
+
+function getSumof2element2(arr, target) { //Time complexity O(n), Space complexity O(1)
+     let result = []
+     let i = 0;
+     let j = arr.length - 1;
+     while (i < j) {
+          if (arr[i] + arr[j] == target) {
+               result.push([arr[i], arr[j]])
+               i++;
+               j--;
+          }
+          else {
+               if (arr[i] + arr[j] < target)
+                    i++;
+               else
+                    j--;
+          }
+     }
+     return result;
+}
+console.log(getSumof2element2(array, targetSum))
